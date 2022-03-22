5347a50798fb7e1a0feca03078fc3280ed11b33a
Renato Alves <alves.rjc@gmail.com>
2022-03-22 15:09:13 +0100

    Avoid yaml warning and load files from path relative to script

--- bin/validate.py
+++ bin/validate.py
@@ -1,13 +1,17 @@
 import sys
+import yaml
+
 from glob import glob
-from yaml import load
+from os.path import abspath, join, dirname, realpath
+
+data_path = abspath(join(dirname(realpath(__file__)), "../_data/members"))
 
 LENGTH = 400
 EXITSTATUS = 0
 
-for file in glob("_data/members/*"):
+for file in glob(f"{data_path}/*"):
     with open(file) as fh:
-        ymlf = load(fh)
+        ymlf = yaml.load(fh, Loader=yaml.FullLoader)
         for key in ymlf:
             if len(ymlf[key]) > 400:
                 print(f"File's '{file}' field '{key}' is larger than 400 characters")
